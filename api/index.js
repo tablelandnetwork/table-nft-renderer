bc1d0415f4eb0659a2f73c228e7feba3811b5873
Allen Muncy <allen@textile.io>
2023-04-11 20:32:36 -0400

    Leggo

--- api/index.js
+++ api/index.js
@@ -49,7 +49,7 @@ app.get("/:chain_id/:table_id", async (req, res, next) => {
     let columns = table_data.schema.columns;
 
     if(extension === "html") {
-      res.sendFile(path.resolve('./public/index.html'), {root: `${__dirname}/../`});
+      res.sendFile(path.join(__dirname + '/../public/index.html'))
       return;
     }
     res.set("Content-Type", "image/svg+xml");
