35e435c986ef54dc6fda26a9f614c9c1a2d21672
Allen Muncy <allen@textile.io>
2023-04-11 20:21:20 -0400

    Dir then parent

--- api/index.js
+++ api/index.js
@@ -49,7 +49,7 @@ app.get("/:chain_id/:table_id", async (req, res, next) => {
     let columns = table_data.schema.columns;
 
     if(extension === "html") {
-      res.sendFile(path.resolve('./public/index.html'), {root: '../'});
+      res.sendFile(path.resolve('./public/index.html'), {root: `${__dirname}/../`});
       return;
     }
     res.set("Content-Type", "image/svg+xml");
