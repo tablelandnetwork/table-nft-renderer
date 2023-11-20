f7a09a88a95bd12ed95e5a42d0ec5758a575a554
Joe Wagner <njwjs722@gmail.com>
2023-11-20 15:27:10 -0700

    update tableland deps, fix chainId typing

--- test/app.test.js
+++ test/app.test.js
@@ -2,7 +2,7 @@ import { describe, test, afterEach, before } from "mocha";
 import { assert } from "sinon";
 import fetch from "node-fetch";
 
-describe("SVG Renderer", function () {
+describe("HTML Service", function () {
   this.timeout("30s");
 
   before(async function () {
@@ -13,9 +13,11 @@ describe("SVG Renderer", function () {
 
   test("Live table returns the HTML app", async function () {
     const svg = await fetch("http://localhost:3000/31337/1.html");
+
     assert.match(svg.status < 300, true);
     assert.match(svg.headers.get("Content-Type"), "text/html; charset=utf-8");
   });
+
   test("Missing table returns 404 for HTML app", async function () {
     const svg = await fetch("http://localhost:3000/31337/9999.html");
     assert.match(svg.status, 404);
